8824fd70567ee7cf92b7f8eb435185ffbb844624
ShinChven <shinchven@gmail.com>
2020-11-16 17:35:57 +0800

    remove lib

--- lib/markdown-header-parser.d.ts
+++
@@ -1,6 +0,0 @@
-interface MarkdownWithHeader {
-    headers?: object | string;
-    markdown?: string;
-}
-export declare const parseMarkdownHeaders: (document: string) => MarkdownWithHeader | undefined;
-export {};
